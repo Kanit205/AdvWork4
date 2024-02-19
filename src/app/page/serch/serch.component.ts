985d54f7f513bef1f57d89ea17423e1616556b8e
Kanit205 <tewson0970593006@gmail.com>
2024-02-20 03:17:28 +0700

    AdvWork4_Fix1

--- src/app/page/serch/serch.component.ts
+++ src/app/page/serch/serch.component.ts
@@ -2,27 +2,41 @@ import { SerchGetResponse } from './../../model/serch_get_res';
 import { Component } from '@angular/core';
 import { OmdbService } from '../../service/api/omdb.service';
 import { OmdbGetResponse } from '../../model/omdb_get_res';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-serch',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './serch.component.html',
   styleUrl: './serch.component.scss'
 })
 export class SerchComponent {
   find: SerchGetResponse | undefined;
+  findID : OmdbGetResponse | undefined;
+  serchBy: number = 0;
 
   constructor(private OmdbService: OmdbService) { }
 
-  async findSerch(s: string, y?: string) {
+  async findSerch(s?: string, y?: string, i?: string) {
     console.log("find...");
-    if (!y) {
-      this.find = await this.OmdbService.getTitle(s);
+    if (s && !y && !i) {
+      this.find = await this.OmdbService.getSerch(s);
+      this.serchBy = 1;
+    } else if (s && y && !i) {
+      this.find = await this.OmdbService.getSerch(s,y);
+      this.serchBy = 1;
     } else {
-      this.find = await this.OmdbService.getTitle(s,y);
+      this.findID = await this.OmdbService.getSerchID(i);
+      this.serchBy = 2;
     }
-    console.log(this.find.Search);
+
+    if (this.serchBy === 1) {
+      console.log(this.find?.Search);
+    } else {
+      console.log(this.findID);
+
+    }
   }
 }
--- src/app/service/api/omdb.service.ts
+++ src/app/service/api/omdb.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
 import { OmdbGetResponse } from '../../model/omdb_get_res';
 import { SerchGetResponse } from '../../model/serch_get_res';
+import { tick } from '@angular/core/testing';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,17 @@ import { SerchGetResponse } from '../../model/serch_get_res';
 export class OmdbService {
   constructor(private constants: Constants, private http: HttpClient) { }
 
-  public async getTitle(s?: string, y?: string) {
+  public async getSerch(s?: string, y?: string) {
     let url = this.constants.API_ENDPOINT + "s=" + s;
     if (y) {
-      url = url + "&y=" + y;
+    url += "&y=" + y;
     }
     const res = await lastValueFrom(this.http.get(url));
     return res as SerchGetResponse;
+  }
+
+  public async getSerchID(i?: string) {
+    const res = await lastValueFrom(this.http.get(`${this.constants.API_ENDPOINT}i=${i}`));
+    return res as OmdbGetResponse;
   }
 }
